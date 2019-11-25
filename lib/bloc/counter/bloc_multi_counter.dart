a981573c29e2f35c502e6a9c2d1016ba9d282972
Triet Bui <x3cafe@gmail.com>
2019-11-25 15:58:57 +0700

    bloc_multi_counter.dart: fix side-effect of using single bloc instance for both left & right MultiCounterBloc

--- lib/bloc/counter/bloc_multi_counter.dart
+++ lib/bloc/counter/bloc_multi_counter.dart
@@ -7,35 +7,24 @@ import 'package:state_management_showcase/util/util_index.dart';
 import 'counter_bloc.dart';
 
 class BlocMultiCounterPage extends StatelessWidget {
+  MultiCounterBloc _leftBloc;
+  MultiCounterBloc _rightBloc;
+
   BlocMultiCounterPage() {
     Fimber.d('init BlocMultiCounterPage');
+    _leftBloc = MultiCounterBloc();
+    _rightBloc = MultiCounterBloc();
   }
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<MultiCounterBloc>(
-      builder: (context) => MultiCounterBloc(),
-      child: _BlocMultiCounterChildPage(),
-    );
-  }
-}
-
-class _BlocMultiCounterChildPage extends StatelessWidget {
-  _BlocMultiCounterChildPage() {
-    Fimber.d('init _BlocMultiCounterChildPage');
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // ignore: close_sinks
-    final multiCounterBloc = BlocProvider.of<MultiCounterBloc>(context);
-
     return Scaffold(
       appBar: AppBar(
         title: Text('flutter_bloc - Counter'),
       ),
       body: TextMultiCounterContainer(
         leftText: BlocBuilder<MultiCounterBloc, MultiCounterState>(
+          bloc: _leftBloc,
           condition: (prevState, state) {
             // Only allow state from left FAB
             return state.isFromLeft;
@@ -46,6 +35,7 @@ class _BlocMultiCounterChildPage extends StatelessWidget {
           },
         ),
         rightText: BlocBuilder<MultiCounterBloc, MultiCounterState>(
+          bloc: _rightBloc,
           condition: (prevState, state) {
             // Only allow state from right FAB
             return !state.isFromLeft;
@@ -61,22 +51,22 @@ class _BlocMultiCounterChildPage extends StatelessWidget {
           tag: 'left',
           onPressIncrease: () {
             Fimber.d('left: onPressIncrease()');
-            multiCounterBloc.add(MultiCounterEvent.increaseLeft);
+            _leftBloc.add(MultiCounterEvent.increaseLeft);
           },
           onPressDecrease: () {
             Fimber.d('left: onPressDecrease()');
-            multiCounterBloc.add(MultiCounterEvent.decreaseLeft);
+            _leftBloc.add(MultiCounterEvent.decreaseLeft);
           },
         ),
         rightFab: FloatingCounterButtons(
           tag: 'right',
           onPressIncrease: () {
             Fimber.d('right: onPressIncrease()');
-            multiCounterBloc.add(MultiCounterEvent.increaseRight);
+            _rightBloc.add(MultiCounterEvent.increaseRight);
           },
           onPressDecrease: () {
             Fimber.d('right: onPressDecrease()');
-            multiCounterBloc.add(MultiCounterEvent.decreaseRight);
+            _rightBloc.add(MultiCounterEvent.decreaseRight);
           },
         ),
       ),
